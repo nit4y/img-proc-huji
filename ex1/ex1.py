8a9c8c9a2b46851196876bce24ea1714d22fda37
Nitay Shoshana <nitay2020@gmail.com>
2024-11-12 20:48:41 +0200

    final ex1

--- ex1/ex1.py
+++ ex1/ex1.py
@@ -5,23 +5,22 @@ from PIL import Image
 
 def threshhold_factory(video_type: int) -> int:
     if video_type == 1:
-        return 10
+        return 1
     elif video_type == 2:
-        return 30
+        return 1.5
     return 0
 
-def main_cumsum(video_path: str, video_type: int) -> tuple:
-    # Parameters
+def main(video_path: str, video_type: int) -> tuple:
+    
     previous_hist = None
-    max_diff = 0
-    max_frame = None
-    max_frame_index = -1
+    threshhold = threshhold_factory(video_type)
+    cuts = []
 
     for frame_index, frame in enumerate(media.read_video(video_path)):
         
         gray_frame = Image.fromarray(frame).convert('L')
         gray_frame = np.array(gray_frame) 
-
+        
         equalized_hist, _ = np.histogram(gray_frame, bins=256, range=(0, 255), density=True)
 
         if previous_hist is not None:
@@ -30,17 +29,14 @@ def main_cumsum(video_path: str, video_type: int) -> tuple:
 
             dist = euclidean(cum_sum_prev, cum_sum_current)
 
-            if dist > max_diff:
-                max_diff = dist
-                max_frame = frame
-                max_frame_index = frame_index
-
+            if dist > threshhold:
+                cuts.append(frame_index)
+                
         # update the previous histogram with the current equalized histogram
         previous_hist = equalized_hist
 
-    return max_frame_index, max_frame, max_diff
+    return tuple(cuts)
 
 if __name__ == "__main__":
-    max_frame_index, max_diff_frame, max_diff = main_cumsum("./video3_category2.mp4", 2)
-    print("Cut in frame:", max_frame_index)
-    print("With diff:", max_diff)
+    cuts = main("./video3_category2.mp4", 1)
+    print("Cuts in frames: ", cuts)
